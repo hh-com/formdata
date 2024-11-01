9f3a7dfcda8ee4d9553d423dabbe58303fcbf545
hh-com <info@harald-huber.com>
2024-11-01 10:07:13 +0100

    upd

--- config/autoload.php
+++ config/autoload.php
@@ -1,15 +1,6 @@
 <?php
 
 /**
- * Contao Open Source CMS
- *
- * Copyright (c) 2005-2018 Leo Feyer
- *
- * @license LGPL-3.0+
- */
-
-
-/**
  * Register the classes
  */
 ClassLoader::addClasses(array
